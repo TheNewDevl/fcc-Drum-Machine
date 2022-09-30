6ab9b2bb93c3c0685c829e30ee1e75217334780c
carlos <carl.devfs@gmail.com>
2022-09-30 15:44:12 +0200

    use hosted sounds

--- src/utils/soundsList.ts
+++ src/utils/soundsList.ts
@@ -3,47 +3,47 @@ import { SoundType } from "./types";
 export const SoundsList: SoundType[] = [
   {
     id: "Heater-1",
-    url: "../public/Heater-1.mp3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
     keyName: "Q",
   },
   {
     id: "Heater-2",
-    url: "../public/Heater-2.mp3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
     keyName: "W",
   },
   {
     id: "Heater-3",
-    url: "../public/Heater-3.mp3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
     keyName: "E",
   },
   {
     id: "Heater-4",
-    url: "../public/Heater-4.mp3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4.mp3",
     keyName: "A",
   },
   {
     id: "Clap",
-    url: "../public/Clap.mp3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
     keyName: "S",
   },
   {
     id: "Closed-HH",
-    url: "../public/Closed-HH.mp3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
     keyName: "D",
   },
   {
     id: "Kick",
-    url: "../public/Kick.mp3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
     keyName: "Z",
   },
   {
     id: "Kick_n_Hat",
-    url: "../public/Kick_n_Hat.mp3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
     keyName: "X",
   },
   {
     id: "Open-HH",
-    url: "../public/Open-HH.mp3",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
     keyName: "C",
   },
 ];
